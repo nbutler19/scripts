9ca7ce8364cf3f5020fce39afea1372ca9613cd8
nbutler <nathan.butler@gmail.com>
2012-10-29 01:29:59 -0400

    Add region support to attach eip script

--- python/attach-eip.py
+++ python/attach-eip.py
@@ -7,8 +7,8 @@ def get_creds():
 
     # By default empty, boto will look for environment variables
     # AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-    ACCESSKEY=''
-    SECRETKEY=''
+    ACCESSKEY=None
+    SECRETKEY=None
 
     return ACCESSKEY, SECRETKEY
 
@@ -18,13 +18,21 @@ def get_args():
                                             help="the instance-id, e.g i-1223456")
     parser.add_argument("-e", "--elastic-ip", dest="eip", required=True,
                                             help="the elastic ipv4 address, e.g. 127.0.0.1")
+    parser.add_argument("-r", "--region", dest="region", default="us-east-1",
+                                            help="the region to use, e.g. us-east-1/us-west-2")
     return parser.parse_args()
 
-def get_conn(accesskey,secretkey):
-    if accesskey and secretkey:
-        return boto.connect_ec2(accesskey,secretkey)
-    else:
-        return boto.connect_ec2()
+def get_conn(args):
+    region = get_region(args)
+    conn = boto.connect_ec2(args.accesskey, args.secretkey, region=region)
+    return conn
+ 
+def get_region(args):
+    import boto.ec2
+    regions = boto.ec2.regions(aws_access_key_id=args.accesskey,aws_secret_access_key=args.secretkey)
+    for region in regions:
+        if args.region == region.name:
+            return region
 
 def get_instance(conn,instanceid):
     reservation = conn.get_all_instances(instanceid)[0]
@@ -46,7 +54,9 @@ def associate_eip(conn,eip,instance):
 def run():
     (accesskey, secretkey) = get_creds()
     args = get_args()
-    conn = get_conn(accesskey, secretkey)
+    args.accesskey = accesskey
+    args.secretkey = secretkey
+    conn = get_conn(args)
     instance = get_instance(conn,args.instanceid)
     eip = get_eip(conn,args.eip)
     associate_eip(conn,eip,instance)
